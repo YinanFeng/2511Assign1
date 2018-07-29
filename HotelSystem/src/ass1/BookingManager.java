da2d99d1f6ec4038d0fb9d96ad0859444c47a2e3
YinanFeng <z5167277@student.unsw.edu.au>
2018-07-29 13:29:03 +1000

    some update

--- HotelSystem/src/ass1/BookingManager.java
+++ HotelSystem/src/ass1/BookingManager.java
@@ -4,6 +4,8 @@ import java.util.ArrayList;
 
 public class BookingManager {
 	private ArrayList<Booking> bookingList = new ArrayList<Booking>();
+	private ArrayList<Room> singleRoomList = new ArrayList<Room>();
+	private ArrayList<Room> doubleRoomList = new ArrayList<Room>();
 	
 	
 	public boolean checkRoomAvailable(String[] bookingInfo) {
@@ -11,18 +13,22 @@ public class BookingManager {
 		
 		for(int i=0;i<newBooking.getOrderList().size();i++) {
 			if(newBooking.getOrderList().get(i).getRoomType().equals("single")) {
-				
+				for(int j=0;j<newBooking.getDuration();j++) {
+					
+				}
 				
 				
 			}
 			if(newBooking.getOrderList().get(i).getRoomType().equals("double")) {
-				
+				for(int m=0;m<newBooking.getDuration();m++) {
+					
+				}
 				
 				
 			}
 			
 			
-			
+			return false;
 		}
 		
 		
@@ -32,9 +38,23 @@ public class BookingManager {
 	
 	
 	
+	public void addNewRoom(String[] roomInfo) {
+		Room newRoom = new Room(roomInfo);
+		if(newRoom.isDouble() == false) {
+			this.singleRoomList.add(newRoom);
+		}
+		
+		if(newRoom.isDouble() == true) {
+			this.doubleRoomList.add(newRoom);
+		}
+	
+	}
+	
 	public void addNewBooking(String[] bookingInfo) {
 		Booking newBooking = new Booking(bookingInfo);
 		bookingList.add(newBooking);
+		
+		
 		
 		//change room(room will be occupid after it)
 	    
